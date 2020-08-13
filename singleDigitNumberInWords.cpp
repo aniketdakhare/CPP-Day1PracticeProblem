919cea6149deb4b0950b1dcbf59479b539675230
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-13 11:06:30 +0530

    ADDED Code to convert single digit to word

---
+++ singleDigitNumberInWords.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    cout << "Enter Number between 0 to 9" << endl;
+    cin >> number;
+    switch (number)
+    {
+    case 0:
+        cout << "Zero";
+        break;
+    case 1:
+        cout << "One";
+        break;
+    case 2:
+        cout << "Two";
+        break;    
+    case 3:
+        cout << "Three";
+        break;
+    case 4:
+        cout << "Four";
+        break;
+    case 5:
+        cout << "Five";
+        break;
+    case 6:
+        cout << "Six";
+        break;
+    case 7:
+        cout << "Seven";
+        break;
+    case 8:
+        cout << "Eight";
+        break;
+    case 9:
+        cout << "Nine";
+        break;
+    default:
+        cout << "Invalid Number";
+        break;
+    }
+    return 0;
+}
